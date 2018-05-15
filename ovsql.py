f25c5a3689aac1314ec8c630035b4ae5b7dbb56c
unknown <139896@dymos.hkmg.global>
2018-05-15 16:06:05 +0900

    adding sql.py

---
+++ ovsql.py
@@ -0,0 +1,29 @@
+#-*- coding: utf-8 -*-
+
+import pymssql
+
+SERVER="10.214.100.73"
+USER="PM_EQ"
+PASSWORD="pm_Eq1@"
+DATABASE="PM_EUQI_NOTI"
+
+def getData(targetDate):
+    SQLCOMMAND="SELECT * FROM PM01_EuqiNotify WHERE notiDate='"+targetDate+"' ORDER BY notiTime DESC"
+    try:
+        conn = pymssql.connect(server=SERVER, user=USER, password=PASSWORD, database=DATABASE)
+    except pymssql.Error as e:
+       print("SQL Error: "+str(e))
+
+    cursor = conn.cursor()
+
+    try:
+        cursor.execute(SQLCOMMAND)
+    except pymssql.Error as e:
+        print("SQL Error: "+str(e))
+        conn.close()
+        exit()
+
+    fetchResult = cursor.fetchall()
+    conn.close()
+
+    return fetchResult
