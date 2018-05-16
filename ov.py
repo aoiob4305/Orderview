2f239c0dafcbbd88baef55b0a4c0e643e1cd8dd1
unknown <139896@dymos.hkmg.global>
2018-05-16 17:30:47 +0900

    'bugfix'

--- ov.py
+++ ov.py
@@ -11,6 +11,8 @@ def main():
 #    results = ovsql.convEncoding(ovsql.getOrderDataTargetDate("20180515"),'ISO-8859-1','euc-kr')
     today = datetime.now().strftime("%Y%m%d")
     status, results = ovsql.getOrderDataTargetDate(today)
+    if not (status is False):
+        results = ovsql.convEncoding(results, 'ISO-8859-1', 'euc-kr')
     return render_template("main.html", results=results)
 
 if __name__ == '__main__':
