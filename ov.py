65bf1c2008299c9cbf369b2cfaa39f5fdd8f9fd8
unknown <139896@dymos.hkmg.global>
2018-05-26 19:55:21 +0900

    adding css

--- ov.py
+++ ov.py
@@ -7,13 +7,21 @@ import ovsql
 app = Flask(__name__)
 
 @app.route("/")
-def main():
-#    results = ovsql.convEncoding(ovsql.getOrderDataTargetDate("20180515"),'ISO-8859-1','euc-kr')
+@app.route("/<plant>")
+def main(plant=None):
     today = datetime.now().strftime("%Y%m%d")
     status, results = ovsql.getOrderDataTargetDate(today)
     if not (status is False):
         results = ovsql.convEncoding(results, 'ISO-8859-1', 'euc-kr')
-    return render_template("main.html", results=results)
+    
+    # 플랜트 데이터 추가
+    resultsAddPlantData = []
+    for result in results:
+        resultsAddPlantData.append(result + (ovsql.getWhichPlant(result[4]),))
+
+    results = resultsAddPlantData
+
+    return render_template("main.html", results=results, plant=plant)
 
 if __name__ == '__main__':
     app.run(port=8888, debug=True)
